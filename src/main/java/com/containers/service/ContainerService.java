62e204d5ea9ca25d51dc61049ae3e8e9dbe0b1c6
Przydan <patryk@przydanek.com>
2020-02-16 15:24:49 +0100

    - fech types in OTM relations in ContainerDamage.

--- src/main/java/com/containers/service/ContainerService.java
+++ src/main/java/com/containers/service/ContainerService.java
@@ -1,5 +1,15 @@
 package com.containers.service;
 
+import com.containers.repository.ContainerDamageRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContainerService {
 
+    private final ContainerDamageRepository containerDamageRepository;
+
+    public ContainerService(ContainerDamageRepository containerDamageRepository) {
+        this.containerDamageRepository = containerDamageRepository;
+        System.out.println("test");
+    }
 }
