3d2fc34643c55140771a73b69cd9eb4fe9f67eb7
Patryk <49061878+Przydan@users.noreply.github.com>
2020-02-17 13:11:29 +0100

    Add container repo and container service  (#24)
    * - remove id from Container .
    - update data.sql
    
    * - fech types in OTM relations in ContainerDamage.
    
    * - container repo + service
    
    * - container repo + service
    - add CRUD methods
    - TODO: findByContainerShipownerId
    
    * - findByContainerShipownerId
    - add tests

--- src/main/java/com/containers/repository/ContainerRepository.java
+++ src/main/java/com/containers/repository/ContainerRepository.java
@@ -4,9 +4,11 @@ import com.containers.model.Container;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface ContainerRepository extends JpaRepository<Container, String> {
 
-    Optional<Container> findById(String noContainer);
+    Optional<Container> findByNoContainer(String noContainer);
 
+    Optional<Set<Container>> findByContainerShipowner_Id(Long id);
 }
