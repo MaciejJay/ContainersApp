0e23cebdcac48a545ff2e4c411d4b341048025c5
MaciejJay <50988314+MaciejJay@users.noreply.github.com>
2020-02-22 13:39:04 +0100

    updated tests for Container Damage Service (#32)

--- src/main/java/com/containers/controller/ContainerController.java
+++ src/main/java/com/containers/controller/ContainerController.java
@@ -3,8 +3,6 @@ package com.containers.controller;
 import com.containers.service.ContainerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
